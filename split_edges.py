d86ed86025c01412499deec4305893294567a4fa
evjlind <evanjlindeman@gwu.edu>
2023-12-14 17:51:48 -0500

    fixed bugs: div_edges, determine_concavity cleaned up unneeded variables

--- split_edges.py
+++ split_edges.py
@@ -1,156 +1,181 @@
-from scipy.spatial.distance import directed_hausdorff as haus_dist
-from scipy import signal
 import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
-import glob, os
-import math
+from shapely.geometry import Polygon as poly
 
-# def cart2pol(x, y):
-#     rho = np.sqrt(x**2 + y**2)
-#     phi = np.arctan2(y, x)
-#     return(rho, phi)
-
-# def pol2cart(rho, phi):
-#     x = rho * np.cos(phi)
-#     y = rho * np.sin(phi)
-#     return(x, y)
-
-img = cv.imread("JigsawTrainingData\MjU-padding.bmp")
-ht,wd = img.shape[:2]
-
-# add padding to img
-old_image_height, old_image_width, channels = img.shape
-
-new_img_w = old_image_width + 10
-new_img_h = old_image_height + 10
-
-result = np.full((new_img_h,new_img_w,channels),(0,0,0),dtype=np.uint8)
-
-x_center = (new_img_w - img.shape[0]) // 2
-y_center = (new_img_h - img.shape[1]) // 2
-
-result[y_center:y_center+img.shape[0], 
-       x_center:x_center+img.shape[1]] = img
-
-img = result
-
-img_gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-# # preprocessing
-# kernel_size = 3
-# kern = np.ones((3,3))
-
-
-# blur_gray = cv.GaussianBlur(img_gray,(kernel_size,kernel_size),0)
-
-# thresh = cv.threshold(blur_gray,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)[1]
-
-# contours, _ = cv.findContours(thresh,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
-
-# m = cv.moments(contours[0])
-# cx = int(m["m10"]/m["m00"])
-# cy = int(m["m01"]/m["m00"])
-
-# polar = [cart2pol(c[0][0] - cx, c[0][1] - cy) for c in contours[0][:]]
-# max_i = polar.index(max(polar, key = lambda x: x[1]))
-# polar = polar[max_i:] + polar[:max_i]
-# ds, phis = zip(*polar)
-
-# ds = signal.medfilt(ds,kernel_size)
-# ds_deriv = np.gradient(ds)
-# b,a = ([1/5,1/5,1/5,1/5,1/5],[1])
-# ds_deriv2 = abs(signal.filtfilt(b,a,ds_deriv))
-# peaks = signal.find_peaks(ds_deriv2,distance=2)
-# corners_x = []
-# corners_y = []
-# for i in peaks[0]:
-#     phi = phis[i]
-#     rho = ds[i]
-#     x,y = pol2cart(rho,phi)
-#     corners_x.append(x+new_img_w/2)
-#     corners_y.append(y+new_img_h/2)
-
-# plt.figure(1)
-# plt.plot(phis,ds_deriv)
-# plt.title('raw polar')
-# plt.figure(2)
-# plt.plot(phis,ds_deriv2)
-# plt.title('filtered polar')
-# plt.figure(3)
-# plt.imshow(thresh)
-# plt.scatter(corners_x,corners_y)
-# plt.figure(4)
-# plt.plot(phis,ds)
-# plt.title('raw-er polar')
-# plt.show()
-
-class getSeparatedEdges():
+class splitEdges():
 
     def get_edges(img):
-
         # add padding to img
-        old_image_height, old_image_width, channels = img.shape
-        new_img_w = old_image_width + 10
-        new_img_h = old_image_height + 10
-        result = np.full((new_img_h,new_img_w,channels),(0,0,0),dtype=np.uint8)
-        x_center = (new_img_w - img.shape[0]) // 2
-        y_center = (new_img_h - img.shape[1]) // 2
+        img, new_img_w, new_img_h = splitEdges.add_padding(img)
 
-        result[y_center:y_center+img.shape[0], 
-            x_center:x_center+img.shape[1]] = img
-        img = result
+        #pre-process
+        #mask out
         img_gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+        _, img_gray = cv.threshold(img_gray,13,255,cv.THRESH_BINARY)
+        img_gray2 = cv.morphologyEx(img_gray, cv.MORPH_CLOSE,(3,3))
+        img_gray2 = cv.morphologyEx(img_gray2,cv.MORPH_OPEN,(3,3))
+        img_gray2 = cv.GaussianBlur(img_gray2,(3,3),0)
+        
+        #corner detection
+        dst = cv.cornerHarris(img_gray2,2,3,0.02) # 0.04, 0.08
 
-        dst = cv.cornerHarris(img_gray,2,3,.04)
-        dst = cv.dilate(dst,None)
-        img[dst>.08*dst.max()] = [0,0,255]
-
-        _, dst = cv.threshold(dst,0.08*dst.max(),255,0)
-        dst = np.uint8(dst)
+        dst[np.where(dst<0)] = 0
+        dst *= 255 # or any coefficient
+        dst = dst.astype(np.uint8)
+        dst = cv.morphologyEx(dst,cv.MORPH_DILATE,(3,3))
+        _, dst = cv.threshold(dst,0,255,cv.THRESH_OTSU+cv.THRESH_BINARY)
         _, _, _, centroids = cv.connectedComponentsWithStats(dst)
-
         centroids = centroids[1:]
-        distances = []
-
-        #find 4 corners that are most remote (rotation invariant)
-
-        #use cv.warpAffine later to guarantee straight lines? rotate
+        border = np.array(cv.Canny(img_gray2,100,200))
+        border_coords = np.argwhere((border>0))
+        #order corners in polar order to properly draw lines
+        polar_corners = []
         for i in centroids:
-            smallest = 300
-            for j in range(len(centroids)):
-                d_x = abs(i[0] - centroids[j][0])
-                d_y = abs(i[1] - centroids[j][1])
-                dist = (d_x**2+d_y**2)**1/2
-                if dist !=0:
-                    if dist < smallest:
-                        smallest = dist
-            distances.append(smallest)
+            polar_corners.append(splitEdges.cart2pol(np.floor(i[0])-new_img_w/2,np.floor(i[1])-new_img_h/2))
+        polar_corners = sorted(polar_corners,key=lambda x:x[1])
+        n=0
+        for i in polar_corners:
+            x,y = splitEdges.pol2cart(i[0],i[1])
+            x,y = x+new_img_w/2,y+new_img_h/2
+            centroids[n] = (x,y)
+            n+=1
+        fin_corners = splitEdges.compute_best_rectangle(centroids,0)
+        #order corners in polar order to properly draw lines
+        polar_corners = []
+        for i in fin_corners:
+            polar_corners.append(splitEdges.cart2pol(np.floor(i[0])+0.5-new_img_w/2,np.floor(i[1])+0.5-new_img_h/2))
+        polar_corners = sorted(polar_corners,key=lambda x:x[1])
+        n=0
+        for i in polar_corners:
+            x,y = splitEdges.pol2cart(i[0],i[1])
+            x,y = x+new_img_w/2,y+new_img_h/2
+            fin_corners[n] = (x,y)
+            n+=1
+        #draw lines between corners
+        temp = np.zeros_like(img_gray2)
+        for i in range(len(fin_corners)-1):
+            p1 = (int(fin_corners[i][0]),int(fin_corners[i][1]))
+            p2 = (int(fin_corners[i+1][0]),int(fin_corners[i+1][1]))
+            temp = cv.line(temp,(int(fin_corners[-1][0]),int(fin_corners[-1][1])),(int(fin_corners[0][0]),int(fin_corners[0][1])),1,1)
+            
+            temp = cv.line(temp,p1,p2,1,1)
 
-        res = sorted(range(len(distances)),key=lambda sub:distances[sub],reverse=True)[:4]
-        fin_corners = centroids[res]
+        temp = cv.line(temp,(int(fin_corners[-1][0]),int(fin_corners[-1][1])),(int(fin_corners[0][0]),int(fin_corners[0][1])),1,1)
 
-        border = cv.Canny(img_gray,100,200)
-        border_coords = np.where(border>0)
+        #for every border pixel, associate it with its edge
+        edges = splitEdges.div_edges(border,fin_corners)
+        edge_images = [np.zeros_like(img_gray),np.zeros_like(img_gray),np.zeros_like(img_gray),np.zeros_like(img_gray)]
+        for j in range(len(edge_images)):
+            coords = edges[j]
+            for i in coords:
+                edge_images[j][i[0][1]][i[0][0]] = 1
+        # find concavity of each side
+        concave,convex,straight = splitEdges.determine_concavity(edge_images,temp)
+        return concave, convex, straight
 
-        for i in range(len(border_coords[0])):
-            x = border_coords[0]
-            y = border_coords[1]
-            for i in range(4):
-                corner = fin_corners[i]
-                
-                
+    def determine_concavity(edges,rectangle):
+        concave,convex,straight = [],[],[]
+        rect_fill = rectangle.copy()
+        h,w = rectangle.shape[:2]
+        mask = np.zeros((h+2,w+2),np.uint8)
+        cv.floodFill(rect_fill,mask,(0,0),(255,255))
+        rect_fill = cv.bitwise_not(rect_fill)
+        areas = []
+        for i in range(len(edges)):
+            coords = np.where(edges[i]==1)
+            area0 = edges[i]
+            area0 = cv.line(edges[i],(coords[1][0],coords[0][0]),(coords[1][-1],coords[0][-1]),1,1)
+            edge_fill = area0.copy()
+            h,w = area0.shape[:2]
+            mask = np.zeros((h+2,w+2),np.uint8)
+            cv.floodFill(edge_fill,mask,(0,0),(255,255))
+            edge_fill = cv.bitwise_not(edge_fill)
+            intersection = cv.bitwise_and(edge_fill,edge_fill,mask=rect_fill)
+            intersection -= area0
+            contours,_ = cv.findContours(intersection, 1, 2)
+            try:
+                cnt = contours[0]
+                areas.append(cv.contourArea(cnt))
+            except:
+                areas.append(0)
+        for i in range(len(areas)):
+            if areas[i]>=max(areas)*0.5:
+                concave.append(edges[i])
+                convex.append([0])
+                straight.append([0])
+            elif areas[i] <= max(areas)*0.2:
+                concave.append([0])
+                convex.append([0])
+                straight.append(edges[i])
+            else:
+                concave.append([0])
+                convex.append(edges[i])
+                straight.append([0])
+        return concave,convex,straight
 
-        # plt.figure(1)
-        # for i in fin_corners:
-        #     plt.scatter(i[0],i[1])
-        # plt.imshow(img)
-        # plt.figure(2)
-        # plt.imshow(dst)
-        # plt.figure(3)
-        # for i in centroids:
-        #     plt.scatter(i[0],i[1])
-        # plt.imshow(img)
-        # plt.title('2')
-        # plt.figure(4)
-        # plt.imshow(border)
-        # plt.show()
+    def cart2pol(x, y):
+        rho = np.sqrt(x**2 + y**2)
+        phi = np.arctan2(y, x)
+        return(rho, phi)
+
+    def pol2cart(rho, phi):
+        x = rho * np.cos(phi)
+        y = rho * np.sin(phi)
+        return(x, y)
+
+    def add_padding(img):
+        _, _, channels = img.shape
+        new_img_w = 150
+        new_img_h = 150
+        result = np.full((new_img_h,new_img_w,channels),(0,0,0),dtype=np.uint8)
+        x_center = (new_img_w - img.shape[1]) // 2
+        y_center = (new_img_h - img.shape[0]) // 2
+
+        result[y_center:y_center+img.shape[0],
+            x_center:x_center+img.shape[1]] = img
+        return result, new_img_w, new_img_h
+
+    def div_edges(border, corners):
+        n_corners = np.zeros_like(corners)
+        for i in range(len(corners)):
+            n_corners[i][0],n_corners[i][1] = int(corners[i][0]),int(corners[i][1])
+        temp = np.zeros_like(border)
+        temp = cv.line(temp,(int(n_corners[0][0])-10,int(n_corners[0][1])-10),(int(n_corners[2][0])+10,int(n_corners[2][1])+10),255,2)
+        temp = cv.line(temp,(int(n_corners[1][0])+10,int(n_corners[1][1])-10),(int(n_corners[3][0])-10,int(n_corners[3][1])+10),255,2)
+        divider = cv.bitwise_not(temp)
+        final = cv.bitwise_and(border,border,mask=divider)
+        n_edges, _ = cv.findContours(final,cv.RETR_TREE,cv.CHAIN_APPROX_NONE)
+        return n_edges
+
+    def compute_best_rectangle(points,toler):
+        max_area = 0
+        for i in range(len(points)):
+            for k in range(i,len(points)):
+                base_vect = (points[i][0]-points[k][0],points[i][1]-points[k][1])
+                for j in range(k,len(points)):
+                    side_vect1 = (points[k][0]-points[j][0],points[k][1]-points[j][1])
+                    theta1 = np.arccos(np.dot(base_vect,side_vect1)/(splitEdges.vector_mag(base_vect)*splitEdges.vector_mag(side_vect1)))
+                    if (1.553 - toler) < theta1 < (1.588 + toler):
+                        for m in range(j,len(points)):
+                            side_vect2 = (points[j][0]-points[m][0],points[j][1]-points[m][1])
+                            side_vect3 = (points[m][0]-points[i][0],points[m][1]-points[i][1])
+                            theta2 = np.arccos(np.dot(side_vect1,side_vect2)/(splitEdges.vector_mag(side_vect2)*splitEdges.vector_mag(side_vect1)))
+                            if (1.553 - toler) < theta2 < (1.588 + toler):
+                                theta3 = np.arccos(np.dot(side_vect2,side_vect3)/(splitEdges.vector_mag(side_vect3)*splitEdges.vector_mag(side_vect2)))
+                                if (1.553 - toler) < theta3 < (1.588 + toler):
+                                        theta4 = np.arccos(np.dot(side_vect3,base_vect)/(splitEdges.vector_mag(side_vect3)*splitEdges.vector_mag(base_vect)))
+                                        if (1.553 - toler) < theta4 < (1.588 + toler):
+                                            x = [points[i][0],points[k][0],points[j][0],points[m][0]]
+                                            y = [points[i][1],points[k][1],points[j][1],points[m][1]]
+                                            rect = poly(zip(x,y))
+                                            if rect.area>max_area:
+                                                best = [points[i],points[j],points[k],points[m]]
+                                                max_area = rect.area
+        if max_area == 0:                                   
+            best = splitEdges.compute_best_rectangle(points,toler+0.001)
+        return best
+                        
+    def vector_mag(vector):
+        return np.sqrt(vector[0]**2+vector[1]**2)
+    
